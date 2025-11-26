cc815ac3ce87f6f3bcd99f02ae5750cee3ab9382
Rabdan <rabdan@itonly.ru>
2025-11-26 14:43:37 +0700

    dashboard docker

--- dashboard-app/nuxt.config.ts
+++ dashboard-app/nuxt.config.ts
@@ -1,5 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
-const mode = process.env.NUXT_PUBLIC_MODE || process.env.NODE_ENV || 'development'
+// https://nuxt.com/docs/api/configuration/nuxt-config
+const mode = process.env.NUXT_PUBLIC_MODE || 'development'
 const isDev = mode !== 'production'
 const n8nBaseUrl = process.env.N8N_BASE_URL || 'http://localhost:5678'
 
